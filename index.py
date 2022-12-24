cdb872a85e8a3b2dcdbbf1f2c00cbc8fcd524d40
Frank-Granjense <121325106+Frank-Granjense@users.noreply.github.com>
2022-12-24 03:03:08 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,13 +1,12 @@
 #!C:/Users/Administrator/AppData/Local/Programs/Python/Python310/python.exe
 from assets import db 
-            
 
 print("Content-Type: text/html\n")
-d = db.Db("country")
-d.query("GovernmentForm", "=", "Republic", "OR")
-d.query("Continent", "=", "Africa", "OR")
-d.query("LifeExpectancy", "<", "45", "AND")
-d.query_order("LifeExpectancy")
-d.query_order("Name","DESC")
+d = db.Db("tabela")
+
+d.query("campo1", "=", "busca1", "AND")
+d.query("campo2", "=", "busca2", "AND")
+d.query("campo3", "<", "3", "OR")
+d.query_order("campo1", "ASC")
+d.query_order("campo3", "DESC")
 print(d.list())
-#print(d.list())
